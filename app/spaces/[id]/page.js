47d71dac8fa718a2f7d81a82583453a886c33e8d
Sumit <sumitgarg380@gmail.com>
2025-01-25 13:36:21 +0530

    asd

--- app/spaces/[id]/page.js
+++ app/spaces/[id]/page.js
@@ -3,26 +3,46 @@ import React, { useEffect } from 'react'
 
 export default function page() {
     const ref = React.useRef(null)
-      const [user, setUser] = React.useState({x: 50, y: 50});
+    const [user, setUser] = React.useState({x: 50, y: 50});
     useEffect(() => {
         const canvas = ref.current
         const ctx = canvas.getContext('2d')
         canvas.width = window.innerWidth
         canvas.height = window.innerHeight
-        //set image as background on canvas
         const img = new Image()
         img.src = '/background.webp'
         img.onload = () => {
             ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
         }
-        if(user && user.x && user.y){
-            const userImg = new Image()
-            userImg.src = '/player.png'
-            userImg.onload = () => {
-                ctx.drawImage(userImg, user.x, user.y, 50, 50)
+
+        const drawUser = () => {
+            if(user && user.x && user.y){
+                const userImg = new Image()
+                userImg.src = '/player.png'
+                userImg.onload = () => {
+                    ctx.clearRect(0, 0, canvas.width, canvas.height)
+                    ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+                    ctx.drawImage(userImg, user.x, user.y, 50, 50)
+                }
             }
         }
 
+        drawUser()
+
+    }, [])
+
+    useEffect(() => {
+        const canvas = ref.current
+        const ctx = canvas.getContext('2d')
+        const img = new Image()
+        img.src = '/background.webp'
+        const userImg = new Image()
+        userImg.src = '/player.png'
+        userImg.onload = () => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height)
+            ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+            ctx.drawImage(userImg, user.x, user.y, 50, 50)
+        }
     }, [user])
 
     const handle = (e) => {

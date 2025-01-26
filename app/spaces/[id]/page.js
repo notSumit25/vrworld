8cfe96513d1d0b5003ce9044a74834d5cff57755
Sumit <sumitgarg380@gmail.com>
2025-01-26 12:56:40 +0530

    Fix

--- app/spaces/[id]/page.js
+++ app/spaces/[id]/page.js
@@ -3,6 +3,7 @@ import { getSocket } from "../../../socket";
 import React, { useEffect, useRef, useState } from "react";
 import { useParams } from "next/navigation";
 import axios from "axios";
+import { useUser } from "@clerk/nextjs";
 
 export default function Page() {
   const { id } = useParams();
@@ -13,25 +14,41 @@ export default function Page() {
     y: 50,
     id: socket.id,
     direction: "down",
+    clerkId: null,
+    name: null,
   });
   const [users, setUsers] = useState(new Map());
   const [direction, setDirection] = useState("down");
   const [map, setMap] = useState(null);
   const [roomUsers, setRoomUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { isLoaded, isSignedIn, user: currUser } = useUser();
+  const [spriteImage, setSpriteImage] = useState("");
 
+  
+  
   const fetchUsers = async () => {
-    const res = await axios.post(`/api/Room/user`,{roomId:id});
+    const res = await axios.post(`/api/Room/user`, { roomId: id });
     setRoomUsers(res.data.users);
-    setMap(res.data.room.Map);
+    setMap(res.data.room.Map.image);
     console.log("Room users", res.data.users);
-    console.log("Map", res.data.room.Map);
- };
-
+    console.log("Map", res.data.room.Map.image);
+    const currUserSprite = res.data.users.find((u) => u.user.clerkId === currUser.id);
+    console.log("Curr user", currUser.id);
+    setUser({ ...user, clerkId: currUser.id, name: currUser.fullName });
+    console.log("Curr user sprite", currUserSprite);
+    if (currUserSprite) {
+      setSpriteImage(currUserSprite.avatar.spiritImage);
+    }
+    setLoading(false); 
+  };
   useEffect(() => {
-    socket.emit("joinSpace", id, user);
-    console.log("Joining space", id);
-    fetchUsers();
-  }, []);
+    if(isLoaded && isSignedIn){
+      socket.emit("joinSpace", id, user);
+      console.log("Joining space", id);
+      fetchUsers();
+    }
+  }, [isLoaded, isSignedIn]);
 
   useEffect(() => {
     socket.on("updateUsers", (users) => {
@@ -41,84 +58,83 @@ export default function Page() {
   }, []);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    if (!loading) {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
 
-    const backgroundImg = new Image();
-    backgroundImg.src = "/background.webp";
+      const backgroundImg = new Image();
+      backgroundImg.src = map;
 
-    const spriteImg = new Image();
-    spriteImg.src = "/sprite.png";
+      const spriteImg = new Image();
+      console.log("Sprite image", spriteImage);
+      spriteImg.src = spriteImage;
 
-    const frameCount = 4;
-    const spriteWidth = spriteImg.width / frameCount;
-    const spriteHeight = spriteImg.height / 4;
+      const frameCount = 4;
+      const spriteWidth = spriteImg.width / frameCount;
+      const spriteHeight = spriteImg.height / 4;
 
-    const draw = () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
+      const draw = () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
 
-      // Draw the current user
-      const directionRow =
-        direction === "down"
-          ? 0
-          : direction === "left"
-          ? 1
-          : direction === "right"
-          ? 2
-          : 3; // 'up'
-      ctx.drawImage(
-        spriteImg,
-        0, // Always the first frame (x = 0)
-        directionRow * spriteHeight, // Source y position
-        spriteWidth, // Source width
-        spriteHeight, // Source height
-        user.x, // Destination x position
-        user.y, // Destination y position
-        50, // Destination width
-        50 // Destination height
-      );
+        // Draw the current user
+        const directionRow =
+          direction === "down"
+            ? 0
+            : direction === "left"
+            ? 1
+            : direction === "right"
+            ? 2
+            : 3; // 'up'
+        ctx.drawImage(
+          spriteImg,
+          0, // Always the first frame (x = 0)
+          directionRow * spriteHeight, // Source y position
+          spriteWidth, // Source width
+          spriteHeight, // Source height
+          user.x, // Destination x position
+          user.y, // Destination y position
+          50, // Destination width
+          50 // Destination height
+        );
 
-      // Draw other users
-      users.forEach((otherUser) => {
-        if (otherUser.id !== socket.id) {
-          const dir =
-            otherUser.direction === "down"
-              ? 0
-              : otherUser.direction === "left"
-              ? 1
-              : otherUser.direction === "right"
-              ? 2
-              : 3; // 'up'
-          ctx.drawImage(
-            spriteImg,
-            0, // Always the first frame (x = 0)
-            dir * spriteHeight, // Source y position
-            spriteWidth, // Source width
-            spriteHeight, // Source height
-            otherUser.x, // Destination x position
-            otherUser.y, // Destination y position
-            50, // Destination width
-            50 // Destination height
-          );
-        }
-      });
+        // Draw other users
+        users.forEach((otherUser) => {
+          if (otherUser.id !== socket.id) {
+            const dir =
+              otherUser.direction === "down"
+                ? 0
+                : otherUser.direction === "left"
+                ? 1
+                : otherUser.direction === "right"
+                ? 2
+                : 3; // 'up'
+            ctx.drawImage(
+              spriteImg,
+              0, // Always the first frame (x = 0)
+              dir * spriteHeight, // Source y position
+              spriteWidth, // Source width
+              spriteHeight, // Source height
+              otherUser.x, // Destination x position
+              otherUser.y, // Destination y position
+              50, // Destination width
+              50 // Destination height
+            );
+          }
+        });
 
-      requestAnimationFrame(draw);
-    };
-    
+        requestAnimationFrame(draw);
+      };
 
+      const handleSpriteLoad = () => {
+        draw();
+      };
 
-    const handleSpriteLoad = () => {
-      draw();
-    };
-
-    spriteImg.onload = handleSpriteLoad;
-    // Remove the initial emit of updateAttributes
-    // socket.emit("updateAttributes", id, user);
-  }, [user, direction, users]);
+      spriteImg.onload = handleSpriteLoad;
+    }
+  }, [user, direction, users, loading, spriteImage]);
 
   const handleKeyDown = (e) => {
     const key = e.key;
@@ -133,7 +149,11 @@ export default function Page() {
       newUser = { ...newUser, x: newUser.x + 20, direction: "right" };
     }
 
-    if (newUser.x !== user.x || newUser.y !== user.y || newUser.direction !== user.direction) {
+    if (
+      newUser.x !== user.x ||
+      newUser.y !== user.y ||
+      newUser.direction !== user.direction
+    ) {
       setDirection(newUser.direction);
       setUser(newUser);
       socket.emit("updateAttributes", id, newUser);
@@ -147,9 +167,18 @@ export default function Page() {
     };
   }, [user]);
 
+  if (!isLoaded || !isSignedIn) {
+    return null;
+  }
   return (
     <div className="min-h-screen bg-gray-50 w-full">
-      <canvas ref={canvasRef} className="w-full h-full" id="canvas"></canvas>
+      {loading ? (
+        <div className="flex items-center justify-center h-full">
+          <p>Loading...</p>
+        </div>
+      ) : (
+        <canvas ref={canvasRef} className="w-full h-full" id="canvas"></canvas>
+      )}
     </div>
   );
 }
